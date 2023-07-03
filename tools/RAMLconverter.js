accb3d1040182c3d1ab95a7c8bf4b5419fa7fda8
Ngoc Quang Nguyen <ngocquang.nguyen@hcl.com>
2023-07-03 08:50:26 +0200

    added env variables

--- tools/RAMLconverter.js
+++ tools/RAMLconverter.js
@@ -13,12 +13,10 @@ myRAMLtoPostman = function (inputPath) {
     function fromDir(startPath, filter) {
 
         console.log('Starting from dir '+startPath+'/');
-    
         if (!fs.existsSync(startPath)) {
-            console.log("no dir ", startPath);
+            console.log("not a legit folder ", startPath);
             return;
         }
-    
         var files = fs.readdirSync(startPath);
         console.log("files are:", files);
         for (var i = 0; i < files.length; i++) {
@@ -38,8 +36,8 @@ myRAMLtoPostman = function (inputPath) {
     const fileName = fullPath.match(regexFileName)[0].replace('/','');
     console.log(fileName);
 
-    const pathWithoutFile = fullPath.replace(fileName,'');
-    console.log(pathWithoutFile);
+    const mainPathWithoutFile = fullPath.replace(fileName,'');
+    console.log(mainPathWithoutFile);
     ramlSpec = fs.readFileSync(fullPath, {encoding: 'UTF8'});
     
 
@@ -55,7 +53,7 @@ myRAMLtoPostman = function (inputPath) {
       });
 
     //adding fragments from files into RAML
-    function addingFragments (ramlSpec, pathWithoutFile){
+    function addingFragments (ramlSpec, mainPathWithoutFile){
         //finding includes
         const regex = /!include.*/g;
         const includeArray = ramlSpec.match(regex);
@@ -64,80 +62,111 @@ myRAMLtoPostman = function (inputPath) {
         //calculate white space
         const regexWholeLine = /^.*!include.*/gm;
         const includeArrayWholeLine = ramlSpec.match(regexWholeLine);
+        console.log('includeArrayWholeLine:', includeArrayWholeLine)
         const includeWhiteSpacesArray = [];
         includeArrayWholeLine.forEach(element => includeWhiteSpacesArray.push(element.length - element.trimStart().length));
 
         
         //read include files and add to a map
         const includeMap = new Map();
-        includeArray.forEach(element => 
+        includeArray.forEach((element, index) => 
             {
                 element = element.trim();
                 var includePath = element.replace('!include', '');
-                var includeFilePath = pathWithoutFile.concat(includePath).replace(' ','');
+                var includePathWithoutFile = includePath.replace(regexFileName,'/');;
+                var includeFilePath = mainPathWithoutFile.concat(includePath).replaceAll(' ','');
+                var includeFilePathWithoutFile = includeFilePath.replace(regexFileName,'/');;
                 if(includeFilePath.includes('../')){
                     includeFilePath = includeFilePath.replace('../','');
                 }
                 ramlFragment = fs.readFileSync(includeFilePath, {encoding: 'UTF8'});
-                includeMap.set(element, ramlFragment)
-            });
-        
-        //replace include with contents from files
-        function replaceIncludeWithFileContent(element, index){
-            element = element.trim();
-            var value = includeMap.get(element);
-            //add fake request
-            if(element.includes('types')){
-                //manipulate white space 
-                const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
-                value = value.replaceAll('\n',paddedNewLine);
-                const regex2 = /(properties.*)/s;
-                value = paddedNewLine + value.match(regex2)[0];
-            }
-            else if(element.includes('example')){
-                const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
-                value = value.replaceAll('\n',paddedNewLine);
-                value = paddedNewLine + value;
-                //console.log('examples:',index , value);
-            }
-            else if (element.includes('traits')){
+                regexTraitsFile = /#%RAML.*/gm;
+                
+                //replace include with contents from files
+                if(ramlFragment.includes('!include')){
+                    const includeFragArray = ramlFragment.match(regex);
+                    includeFragArray.forEach((includeElement, includeIndex) => 
+                    {
+                        console.log('checking includes',includeIndex , includeElement);
+                        includeElementPath = mainPathWithoutFile.concat(includeElement.replace('!include ',''));
+                        if(fs.existsSync(includeElementPath)){
+                            console.log('True!');
+                        }
+                        else{
+                            console.log('false');
+                            console.log('includePath: ', includeElementPath);
+                            if(!includeElement.includes('..')){
+                                includeElementPath = includePathWithoutFile.concat(includeElement.replace('!include ',''));
+                                console.log('includePath after: ', includeElementPath);
+                                ramlFragment = ramlFragment.replace(includeElement,'!include ' + includeElementPath);
+                            }
+                        }
+                    });              
+                }
+                    if(element.includes('types')|ramlFragment.includes('DataType')){
+                        //manipulate white space 
+                        const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
+                        ramlFragment = ramlFragment.replaceAll('\n',paddedNewLine);
+                        const regex2 = /(properties.*)/s;
+                        if(ramlFragment.match(regex2) != null){
+                            ramlFragment = paddedNewLine + ramlFragment.match(regex2)[0];
+                        }
+                    }
+                    else if(element.includes('example')){
+                        const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
+                        ramlFragment = ramlFragment.replaceAll('\n',paddedNewLine);
+                        ramlFragment = paddedNewLine + ramlFragment;
+                    }
+                    else if (element.includes('traits')|ramlFragment.includes('Trait')){
+                        //manipulate white space 
+                        const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
+                        ramlFragment = ramlFragment.replaceAll('\n',paddedNewLine);
+                    }
+                    else{
+                        //not replacing 
+                        console.log('others (not replacing ):',index , ramlFragment);
+                        ramlFragment = element; 
+                    }
                 //remove first line
-                regexTraitsFile = /#%RAML.*/gm;  
-                value = value.replace(regexTraitsFile,'');
-                value = value.replace('\n','');
-                const paddedNewLine = '\n'.padEnd(includeWhiteSpacesArray[index]+2,' ');
-                value = value.replaceAll('\n',paddedNewLine);
-            }
-            else {
-                //replacing 
-                console.log('others:',index , value);
-            }
-            ramlSpec = ramlSpec.replace(element,value);
-        };
-        includeArray.forEach(replaceIncludeWithFileContent);
-        //console.log(ramlSpec);
-  
-
+                ramlFragment = ramlFragment.replace(regexTraitsFile,'');   
+                ramlSpec = ramlSpec.replace(element,ramlFragment);
+            });
         return ramlSpec;
     }
 
     //replace all includes
-    var newRamlSpec = addingFragments(ramlSpec,pathWithoutFile);
+    var newRamlSpec = addingFragments(ramlSpec,mainPathWithoutFile);
     while(newRamlSpec.includes('!include')){
-        newRamlSpec = addingFragments(newRamlSpec,pathWithoutFile);
+        newRamlSpec = addingFragments(newRamlSpec,mainPathWithoutFile);
     }
 
-    console.log(newRamlSpec);
     //add this request for a workaround of the bug
-    fakeRequest = '/fakequest: \n  post:\n    body:\n    responses:\n      200:\n        body:\n          application/json:\n            example: {"code": 200,"status": "OK"}\n\n';
-    //regexFirstRequest = /^\s*\//gm;
+    fakeRequest = '/fakequest: \n  post:\n    body:\n     application/json:\n    responses:\n      200:\n        body:\n          application/json:\n            example: {"code": 200,"status": "OK"}\n\n';
     regexFirstRequest = /^\s*\/.*/gm;
     firstRequest = newRamlSpec.match(regexFirstRequest)[0];
-    //console.log(firstRequest);
     newRamlSpec = newRamlSpec.replace(firstRequest,fakeRequest + firstRequest);
-    console.log(newRamlSpec);
+    /*fs.writeFile('newRamlSpec.RAML',newRamlSpec,(err) => {
+        if (err) { console.log(err); }
+        console.log('newRamlSpec saved');
+    });*/
 
-    //console.log(addingFragments(ramlSpec,pathWithoutFile));
+    function replaceObjectValue(obj){
+        for(const key of Object.keys(obj)){
+         if (typeof obj[key] === 'object'){
+            replaceObjectValue(obj[key]);
+         }
+         if (key === 'header'){
+            if(obj[key] != null){
+                obj[key].forEach((element, index) =>{
+                    if(element.key != 'Content-Type'){
+                        element.value = '{{'+element.key+'}}' 
+                    }
+                })
+            }
+         }
+        }
+    };
+
     //convert raml to postman collection
     Converter.convert({ type: 'string', data: newRamlSpec, options:
         {
@@ -155,6 +184,7 @@ myRAMLtoPostman = function (inputPath) {
             console.log('The collection object is: ', conversionResult.output[0].data);
             postmanCollection.items.add(conversionResult.output[0].data);
             const collectionJSON = postmanCollection.toJSON();
+            replaceObjectValue(collectionJSON);
             fs.writeFile(fileName.replace('.raml','') + '-collection' + '.json', JSON.stringify(collectionJSON), (err) => {
                 if (err) { console.log(err); }
                 console.log('File saved');
